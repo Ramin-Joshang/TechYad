2b06879182d8c71902688cc12ef7ef1c3fbbcc94
Ramin Joshang <91675032+Ramin-Joshang@users.noreply.github.com>
2026-09-18 12:52:05 +0330

    delete test file

--- fix_instructor_list_query.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('frontend/src/components/instructors/InstructorsList.tsx', 'utf8');
-
-code = code.replace(
-  /queryFn: \(\) => api\.get\('\/instructors'\)\.then\(res => res\.data\.data\)/,
-  "queryFn: () => api.get('/instructors').then((res: any) => res.data)"
-);
-
-fs.writeFileSync('frontend/src/components/instructors/InstructorsList.tsx', code);
--- fix_instructor_query.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('frontend/src/components/instructors/InstructorProfileContainer.tsx', 'utf8');
-
-code = code.replace(
-  /queryFn: \(\) => api\.get\(\`\/instructors\/\$\{id\}\`\)\.then\(res => res\.data\.data\)/,
-  'queryFn: () => api.get(`/instructors/${id}`).then((res: any) => res.data)'
-);
-
-fs.writeFileSync('frontend/src/components/instructors/InstructorProfileContainer.tsx', code);
